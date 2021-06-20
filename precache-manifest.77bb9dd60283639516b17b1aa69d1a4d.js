289a4e44c5783495833c739358533d3c5218b3a6
OleksandrLisnevskyi <lisnevskyi@gmail.com>
2021-06-20 17:45:39 +0300

    Updates

---
+++ precache-manifest.77bb9dd60283639516b17b1aa69d1a4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d50ffd0c8292fa4422ff6e428ef950",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a0f43689a3d119e03555",
+    "url": "./static/css/main.db4f92f8.chunk.css"
+  },
+  {
+    "revision": "7c7a19eaf806aa4e071d",
+    "url": "./static/js/2.cad20d42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cad20d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0f43689a3d119e03555",
+    "url": "./static/js/main.e91ca39e.chunk.js"
+  },
+  {
+    "revision": "2a286f7172708a7e8c5a",
+    "url": "./static/js/runtime-main.3a2c9fb2.js"
+  }
+]);
